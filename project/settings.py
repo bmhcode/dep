9dade41cd17b16572462bd794b4f484c4aa64274
bmhcode <51011661+bmhcode@users.noreply.github.com>
2023-12-04 09:18:15 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -119,15 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
-
-#STATIC_URL = '/static/' # Location of static files
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
